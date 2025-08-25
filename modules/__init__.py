257bcf3bcd5db46caf7784f31e66a8b0b584b324
Developer <dev@company.com>
2025-08-25 15:43:58 +0300

    Исправление импортов в modules/__init__.py
    - Убраны несуществующие импорты функций и классов
    - Оставлены только реально существующие модули
    - Скрипт теперь запускается без ошибок

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,23 +5,11 @@
 from .selenium_helpers import get_driver, setup_proxy, apply_cdp_download_settings
 from .new_site_handler import NewSiteReportHandler
 from .page_analyzer import PageAnalyzer
-from .data_processing import process_data
-from .excel_manager import ExcelManager
-from .download_manager import DownloadManager
-from .date_time_utils import DateUtils
-from .regions import RegionManager
-from .skills import SkillsManager
 
 __all__ = [
     'get_driver',
-    'setup_proxy',
+    'setup_proxy', 
     'apply_cdp_download_settings',
     'NewSiteReportHandler',
-    'PageAnalyzer',
-    'process_data',
-    'ExcelManager',
-    'DownloadManager',
-    'DateUtils',
-    'RegionManager',
-    'SkillsManager'
+    'PageAnalyzer'
 ]
